d15dc636c7180deae35ec07fa55a961a5288b2d4
Wahyu2204 <wahyuazza12312@gmail.com>
2025-06-22 21:18:50 +0700

    wahyu commit

--- frontend/src/components/LandingPages/fitur.jsx
+++ frontend/src/components/LandingPages/fitur.jsx
@@ -4,49 +4,87 @@ export default function Fitur() {
       <div class="py-8 px-4 mx-auto max-w-screen-xl sm:py-16 lg:px-6">
         <div class="max-w-screen-md mb-8 lg:mb-16">
           <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">
-            Fitur unggulan  yang kami tawarkan
+            Fitur unggulan yang kami tawarkan
           </h2>
           <p class="text-gray-500 sm:text-xl dark:text-gray-400">
-             Dengan MyInventory, semua proses mulai dari registrasi pengguna, peminjaman alat, 
-             hingga pengembalian dicatat secara digital dan dapat diakses dengan mudah sesuai peran masing-masing pengguna.
+            Dengan MyInventory, semua proses mulai dari registrasi pengguna,
+            peminjaman alat, hingga pengembalian dicatat secara digital dan
+            dapat diakses dengan mudah sesuai peran masing-masing pengguna.
           </p>
         </div>
         <div class="space-y-8 md:grid md:grid-cols-2 lg:grid-cols-3 md:gap-12 md:space-y-0">
           <div>
             <div class="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-blue-700 lg:h-12 lg:w-12 dark:bg-primary-900">
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
-            <path fill-rule="evenodd" d="M12 1.5a5.25 5.25 0 0 0-5.25 5.25v3a3 3 0 0 0-3 3v6.75a3 3 0 0 0 3 3h10.5a3 3 0 0 0 3-3v-6.75a3 3 0 0 0-3-3v-3c0-2.9-2.35-5.25-5.25-5.25Zm3.75 8.25v-3a3.75 3.75 0 1 0-7.5 0v3h7.5Z" clip-rule="evenodd" />
-          </svg>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="currentColor"
+                class="size-6"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M12 1.5a5.25 5.25 0 0 0-5.25 5.25v3a3 3 0 0 0-3 3v6.75a3 3 0 0 0 3 3h10.5a3 3 0 0 0 3-3v-6.75a3 3 0 0 0-3-3v-3c0-2.9-2.35-5.25-5.25-5.25Zm3.75 8.25v-3a3.75 3.75 0 1 0-7.5 0v3h7.5Z"
+                  clip-rule="evenodd"
+                />
+              </svg>
             </div>
-            <h3 class="mb-2 text-xl font-bold dark:text-white">Autentikasi dan Manajemen Pengguna</h3>
+            <h3 class="mb-2 text-xl font-bold dark:text-white">
+              Autentikasi dan Manajemen Pengguna
+            </h3>
             <p class="text-gray-500 dark:text-gray-400">
               Kelola akses pengguna dengan aman.
             </p>
             <p class="text-gray-500 dark:text-gray-400">
-              Anda dapat login ke sistem menggunakan kredensial masing-masing. 
-              Sistem memverifikasi data saat login maupun saat pendaftaran akun baru.
+              Anda dapat login ke sistem menggunakan kredensial masing-masing.
+              Sistem memverifikasi data saat login maupun saat pendaftaran akun
+              baru.
             </p>
           </div>
           <div>
             <div class="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-blue-700 lg:h-12 lg:w-12 dark:bg-primary-900">
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
-            <path fill-rule="evenodd" d="M7.502 6h7.128A3.375 3.375 0 0 1 18 9.375v9.375a3 3 0 0 0 3-3V6.108c0-1.505-1.125-2.811-2.664-2.94a48.972 48.972 0 0 0-.673-.05A3 3 0 0 0 15 1.5h-1.5a3 3 0 0 0-2.663 1.618c-.225.015-.45.032-.673.05C8.662 3.295 7.554 4.542 7.502 6ZM13.5 3A1.5 1.5 0 0 0 12 4.5h4.5A1.5 1.5 0 0 0 15 3h-1.5Z" clip-rule="evenodd" />
-            <path fill-rule="evenodd" d="M3 9.375C3 8.339 3.84 7.5 4.875 7.5h9.75c1.036 0 1.875.84 1.875 1.875v11.25c0 1.035-.84 1.875-1.875 1.875h-9.75A1.875 1.875 0 0 1 3 20.625V9.375ZM6 12a.75.75 0 0 1 .75-.75h.008a.75.75 0 0 1 .75.75v.008a.75.75 0 0 1-.75.75H6.75a.75.75 0 0 1-.75-.75V12Zm2.25 0a.75.75 0 0 1 .75-.75h3.75a.75.75 0 0 1 0 1.5H9a.75.75 0 0 1-.75-.75ZM6 15a.75.75 0 0 1 .75-.75h.008a.75.75 0 0 1 .75.75v.008a.75.75 0 0 1-.75.75H6.75a.75.75 0 0 1-.75-.75V15Zm2.25 0a.75.75 0 0 1 .75-.75h3.75a.75.75 0 0 1 0 1.5H9a.75.75 0 0 1-.75-.75ZM6 18a.75.75 0 0 1 .75-.75h.008a.75.75 0 0 1 .75.75v.008a.75.75 0 0 1-.75.75H6.75a.75.75 0 0 1-.75-.75V18Zm2.25 0a.75.75 0 0 1 .75-.75h3.75a.75.75 0 0 1 0 1.5H9a.75.75 0 0 1-.75-.75Z" clip-rule="evenodd" />
-          </svg>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="currentColor"
+                class="size-6"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M7.502 6h7.128A3.375 3.375 0 0 1 18 9.375v9.375a3 3 0 0 0 3-3V6.108c0-1.505-1.125-2.811-2.664-2.94a48.972 48.972 0 0 0-.673-.05A3 3 0 0 0 15 1.5h-1.5a3 3 0 0 0-2.663 1.618c-.225.015-.45.032-.673.05C8.662 3.295 7.554 4.542 7.502 6ZM13.5 3A1.5 1.5 0 0 0 12 4.5h4.5A1.5 1.5 0 0 0 15 3h-1.5Z"
+                  clip-rule="evenodd"
+                />
+                <path
+                  fill-rule="evenodd"
+                  d="M3 9.375C3 8.339 3.84 7.5 4.875 7.5h9.75c1.036 0 1.875.84 1.875 1.875v11.25c0 1.035-.84 1.875-1.875 1.875h-9.75A1.875 1.875 0 0 1 3 20.625V9.375ZM6 12a.75.75 0 0 1 .75-.75h.008a.75.75 0 0 1 .75.75v.008a.75.75 0 0 1-.75.75H6.75a.75.75 0 0 1-.75-.75V12Zm2.25 0a.75.75 0 0 1 .75-.75h3.75a.75.75 0 0 1 0 1.5H9a.75.75 0 0 1-.75-.75ZM6 15a.75.75 0 0 1 .75-.75h.008a.75.75 0 0 1 .75.75v.008a.75.75 0 0 1-.75.75H6.75a.75.75 0 0 1-.75-.75V15Zm2.25 0a.75.75 0 0 1 .75-.75h3.75a.75.75 0 0 1 0 1.5H9a.75.75 0 0 1-.75-.75ZM6 18a.75.75 0 0 1 .75-.75h.008a.75.75 0 0 1 .75.75v.008a.75.75 0 0 1-.75.75H6.75a.75.75 0 0 1-.75-.75V18Zm2.25 0a.75.75 0 0 1 .75-.75h3.75a.75.75 0 0 1 0 1.5H9a.75.75 0 0 1-.75-.75Z"
+                  clip-rule="evenodd"
+                />
+              </svg>
             </div>
-            <h3 class="mb-2 text-xl font-bold dark:text-white">Registrasi Peminjam</h3>
+            <h3 class="mb-2 text-xl font-bold dark:text-white">
+              Registrasi Peminjam
+            </h3>
             <p class="text-gray-500 dark:text-gray-400">
               Pendaftaran mudah dan cepat.
             </p>
             <p class="text-gray-500 dark:text-gray-400">
-              Anda dapat mendaftar dengan mengisi nama, email, dan password. Sistem akan memvalidasi data sebelum akun aktif digunakan.
+              Anda dapat mendaftar dengan mengisi nama, email, dan password.
+              Sistem akan memvalidasi data sebelum akun aktif digunakan.
             </p>
           </div>
           <div>
             <div class="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-blue-700 lg:h-12 lg:w-12 dark:bg-primary-900">
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
-            <path fill-rule="evenodd" d="M7.5 6a4.5 4.5 0 1 1 9 0 4.5 4.5 0 0 1-9 0ZM3.751 20.105a8.25 8.25 0 0 1 16.498 0 .75.75 0 0 1-.437.695A18.683 18.683 0 0 1 12 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 0 1-.437-.695Z" clip-rule="evenodd" />
-          </svg>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="currentColor"
+                class="size-6"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M7.5 6a4.5 4.5 0 1 1 9 0 4.5 4.5 0 0 1-9 0ZM3.751 20.105a8.25 8.25 0 0 1 16.498 0 .75.75 0 0 1-.437.695A18.683 18.683 0 0 1 12 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 0 1-.437-.695Z"
+                  clip-rule="evenodd"
+                />
+              </svg>
             </div>
             <h3 class="mb-2 text-xl font-bold dark:text-white">
               Kelola Profil Pengguna
@@ -55,48 +93,76 @@ export default function Fitur() {
               Update data pribadi kapan saja.
             </p>
             <p class="text-gray-500 dark:text-gray-400">
-              Anda dapat mengedit informasi seperti nama, email, dan password. Admin juga dapat mengelola akun pengguna lainnya jika dibutuhkan.
+              Anda dapat mengedit informasi seperti nama, email, dan password.
+              Admin juga dapat mengelola akun pengguna lainnya jika dibutuhkan.
             </p>
           </div>
           <div>
             <div class="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-blue-700 lg:h-12 lg:w-12 dark:bg-primary-900">
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
-            <path d="M12.378 1.602a.75.75 0 0 0-.756 0L3 6.632l9 5.25 9-5.25-8.622-5.03ZM21.75 7.93l-9 5.25v9l8.628-5.032a.75.75 0 0 0 .372-.648V7.93ZM11.25 22.18v-9l-9-5.25v8.57a.75.75 0 0 0 .372.648l8.628 5.033Z" />
-          </svg>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="currentColor"
+                class="size-6"
+              >
+                <path d="M12.378 1.602a.75.75 0 0 0-.756 0L3 6.632l9 5.25 9-5.25-8.622-5.03ZM21.75 7.93l-9 5.25v9l8.628-5.032a.75.75 0 0 0 .372-.648V7.93ZM11.25 22.18v-9l-9-5.25v8.57a.75.75 0 0 0 .372.648l8.628 5.033Z" />
+              </svg>
             </div>
-            <h3 class="mb-2 text-xl font-bold dark:text-white">Proses Peminjaman Alat</h3>
+            <h3 class="mb-2 text-xl font-bold dark:text-white">
+              Proses Peminjaman Alat
+            </h3>
             <p class="text-gray-500 dark:text-gray-400">
               Ajukan dan kelola peminjaman dengan mudah.
             </p>
             <p class="text-gray-500 dark:text-gray-400">
-              Anda dapat memilih alat yang ingin dipinjam. 
+              Anda dapat memilih alat yang ingin dipinjam.
             </p>
           </div>
           <div>
             <div class="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-blue-700 lg:h-12 lg:w-12 dark:bg-primary-900">
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
-            <path fill-rule="evenodd" d="M4.755 10.059a7.5 7.5 0 0 1 12.548-3.364l1.903 1.903h-3.183a.75.75 0 1 0 0 1.5h4.992a.75.75 0 0 0 .75-.75V4.356a.75.75 0 0 0-1.5 0v3.18l-1.9-1.9A9 9 0 0 0 3.306 9.67a.75.75 0 1 0 1.45.388Zm15.408 3.352a.75.75 0 0 0-.919.53 7.5 7.5 0 0 1-12.548 3.364l-1.902-1.903h3.183a.75.75 0 0 0 0-1.5H2.984a.75.75 0 0 0-.75.75v4.992a.75.75 0 0 0 1.5 0v-3.18l1.9 1.9a9 9 0 0 0 15.059-4.035.75.75 0 0 0-.53-.918Z" clip-rule="evenodd" />
-          </svg>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="currentColor"
+                class="size-6"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M4.755 10.059a7.5 7.5 0 0 1 12.548-3.364l1.903 1.903h-3.183a.75.75 0 1 0 0 1.5h4.992a.75.75 0 0 0 .75-.75V4.356a.75.75 0 0 0-1.5 0v3.18l-1.9-1.9A9 9 0 0 0 3.306 9.67a.75.75 0 1 0 1.45.388Zm15.408 3.352a.75.75 0 0 0-.919.53 7.5 7.5 0 0 1-12.548 3.364l-1.902-1.903h3.183a.75.75 0 0 0 0-1.5H2.984a.75.75 0 0 0-.75.75v4.992a.75.75 0 0 0 1.5 0v-3.18l1.9 1.9a9 9 0 0 0 15.059-4.035.75.75 0 0 0-.53-.918Z"
+                  clip-rule="evenodd"
+                />
+              </svg>
             </div>
             <h3 class="mb-2 text-xl font-bold dark:text-white">
-             Pengembalian Alat
+              Pengembalian Alat
             </h3>
             <p class="text-gray-500 dark:text-gray-400">
               Pastikan alat kembali tepat waktu.
             </p>
             <p class="text-gray-500 dark:text-gray-400">
-             Anda dapat mengajukan pengembalian. 
-             Sistem mencatat tanggal dan mengubah status alat kembali menjadi "Tersedia".
+              Anda dapat mengajukan pengembalian. Sistem mencatat tanggal dan
+              mengubah status alat kembali menjadi "Tersedia".
             </p>
           </div>
           <div>
             <div class="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-blue-700 lg:h-12 lg:w-12 dark:bg-primary-900">
-             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
-            <path d="M5.85 3.5a.75.75 0 0 0-1.117-1 9.719 9.719 0 0 0-2.348 4.876.75.75 0 0 0 1.479.248A8.219 8.219 0 0 1 5.85 3.5ZM19.267 2.5a.75.75 0 1 0-1.118 1 8.22 8.22 0 0 1 1.987 4.124.75.75 0 0 0 1.48-.248A9.72 9.72 0 0 0 19.266 2.5Z" />
-            <path fill-rule="evenodd" d="M12 2.25A6.75 6.75 0 0 0 5.25 9v.75a8.217 8.217 0 0 1-2.119 5.52.75.75 0 0 0 .298 1.206c1.544.57 3.16.99 4.831 1.243a3.75 3.75 0 1 0 7.48 0 24.583 24.583 0 0 0 4.83-1.244.75.75 0 0 0 .298-1.205 8.217 8.217 0 0 1-2.118-5.52V9A6.75 6.75 0 0 0 12 2.25ZM9.75 18c0-.034 0-.067.002-.1a25.05 25.05 0 0 0 4.496 0l.002.1a2.25 2.25 0 1 1-4.5 0Z" clip-rule="evenodd" />
-          </svg>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="currentColor"
+                class="size-6"
+              >
+                <path d="M5.85 3.5a.75.75 0 0 0-1.117-1 9.719 9.719 0 0 0-2.348 4.876.75.75 0 0 0 1.479.248A8.219 8.219 0 0 1 5.85 3.5ZM19.267 2.5a.75.75 0 1 0-1.118 1 8.22 8.22 0 0 1 1.987 4.124.75.75 0 0 0 1.48-.248A9.72 9.72 0 0 0 19.266 2.5Z" />
+                <path
+                  fill-rule="evenodd"
+                  d="M12 2.25A6.75 6.75 0 0 0 5.25 9v.75a8.217 8.217 0 0 1-2.119 5.52.75.75 0 0 0 .298 1.206c1.544.57 3.16.99 4.831 1.243a3.75 3.75 0 1 0 7.48 0 24.583 24.583 0 0 0 4.83-1.244.75.75 0 0 0 .298-1.205 8.217 8.217 0 0 1-2.118-5.52V9A6.75 6.75 0 0 0 12 2.25ZM9.75 18c0-.034 0-.067.002-.1a25.05 25.05 0 0 0 4.496 0l.002.1a2.25 2.25 0 1 1-4.5 0Z"
+                  clip-rule="evenodd"
+                />
+              </svg>
             </div>
-            <h3 class="mb-2 text-xl font-bold dark:text-white">Notifikasi Otomatis</h3>
+            <h3 class="mb-2 text-xl font-bold dark:text-white">
+              Notifikasi Otomatis
+            </h3>
             <p class="text-gray-500 dark:text-gray-400">
               Jangan lewatkan aktivitas penting.
             </p>
